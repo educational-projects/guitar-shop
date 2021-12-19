f6692f91644e979333131a72c4d6cc8f934f993b
ваше имя <dimarik-tixonov@yandex.ru>
2021-12-19 20:09:57 +0300

    add: отрисует полученный с сервера список гитар

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,27 +1,40 @@
-function Card(): JSX.Element {
+import { nanoid } from '@reduxjs/toolkit';
+import { Guitar } from '../../types/guitar';
+
+type CardProps = {
+  guitar: Guitar
+}
+
+const RATING_LENGTH = 5;
+
+function Card({guitar}: CardProps): JSX.Element {
+  const {previewImg, name, price, rating} = guitar;
+
   return (
     <div className="product-card">
-      <img src="img/content/guitar-2.jpg" width="75" height="190" alt="СURT Z30 Plus Acoustics" />
+      <img src={`img/content/${previewImg.slice(4)}`} width="75" height="190" alt={name} />
       <div className="product-card__info">
-        <div className="rate product-card__rate" aria-hidden="true"><span className="visually-hidden">Рейтинг:</span>
-          <svg width="12" height="11" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
-          </svg>
-          <svg width="12" height="11" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
-          </svg>
-          <svg width="12" height="11" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
-          </svg>
-          <svg width="12" height="11" aria-hidden="true">
-            <use xlinkHref="#icon-full-star"></use>
-          </svg>
-          <svg width="12" height="11" aria-hidden="true">
-            <use xlinkHref="#icon-star"></use>
-          </svg><span className="rate__count">9</span><span className="rate__message"></span>
+        <div className="rate product-card__rate" aria-hidden="true">
+          <span className="visually-hidden">Рейтинг:</span>
+          {new Array(Math.round(rating)).fill(null).map(() => (
+            <svg key={nanoid()} width="12" height="11" aria-hidden="true">
+              <use xlinkHref="#icon-full-star"></use>
+            </svg>
+          ))}
+          {new Array(RATING_LENGTH - Math.round(rating)).fill(null).map(() => (
+            <svg key={nanoid()} width="12" height="11" aria-hidden="true">
+              <use xlinkHref="#icon-star"></use>
+            </svg>
+          ))}
+          <span className="rate__count">9</span>
+          <span className="rate__message"></span>
         </div>
-        <p className="product-card__title">СURT Z30 Plus Acoustics</p>
-        <p className="product-card__price"><span className="visually-hidden">Цена:</span>129 500 ₽
+        <p className="product-card__title">{name}</p>
+        <p className="product-card__price">
+          <span className="visually-hidden">
+            Цена:
+          </span>
+          {`${price} ₽`}
         </p>
       </div>
       <div className="product-card__buttons">
--- src/components/cards-list/cards-list.tsx
+++ src/components/cards-list/cards-list.tsx
@@ -1,9 +1,18 @@
+import { useSelector } from 'react-redux';
+import { getGuitars } from '../../store/products/selectors';
 import Card from '../card/card';
 
 function CardsList(): JSX.Element {
+  const guitars = useSelector(getGuitars);
+
   return (
     <div className="cards catalog__cards">
-      <Card/>
+      {guitars.map((guitar) => (
+        <Card
+          guitar={guitar}
+          key={guitar.id}
+        />
+      ))}
     </div>
   );
 }
